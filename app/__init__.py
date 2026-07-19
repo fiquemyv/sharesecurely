815eb18dee1c150b21aa61bc0c0c7629e69739ce
Muyivu Shafiq <cybersharph@gmail.com>
2026-07-20 01:03:14 +0300

    configure and connect to database

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,17 @@
 from flask import Flask
-import dotenv
+
+from app.config import Config
+from app.extensions import db
+
 
 def create_app():
     app = Flask(__name__)
 
-    app.config["SECRET_KEY"] = dotenv.get_key(".env", "SECRET_KEY")
+    app.config.from_object(Config)
+
+    db.init_app(app)
 
     from app.routes import main
-
     app.register_blueprint(main)
 
     return app
